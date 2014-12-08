efd41812e773e6926833013cbe1af2e19b0e9489
Anthony <aavina2@gmail.com>
2014-12-08 01:33:54 -0600

    Easy configuration

--- src/binarypebl.c
+++ src/binarypebl.c
@@ -7,36 +7,42 @@ static Layer *layer;
 //static int PEB_RES_X = 144;
 static int PEB_RES_Y = 168;
 
-static int OUTER_BOX_SIZE = 10;
-static int INNER_BOX_SIZE = 6;
-static int BOX_OFFSET = 2;
+static int OUTER_BOX_SIZE = 14; // Size of Bit box (outside)
+static int INNER_BOX_SIZE; // Size of Bit box (inside)
+static int BOX_OFFSET;
+static int X_OFFSET = 21; // Where to start drawing the Bits - x
+static int Y_OFFSET = 40; // Where to start drawing the Bits - y
 
 typedef struct {
-  int x;
-  int y;
+  int x; /* x-coord of origin */
+  int y; /* y-coord of origin */
   int on; /* 0 = off, anything else = on */
 } Bit;
 
-static Bit g_h1[2];
-static Bit g_h0[4];
-static Bit g_m1[3];
-static Bit g_m0[4];
+static Bit g_h1[2]; // Bits for hour1 digit
+static Bit g_h0[4]; // Bits for hour0 digit
+static Bit g_m1[3]; // Bits for minute1 digit
+static Bit g_m0[4]; // Bits for minute0 digit
 
+// Bit-masks to find out which bits are set
 static const int g_bitmasks[] = {0x01, 0x02, 0x04, 0x08};
 
+// Will draw an 'on' bit, or a square that's filled in
 static void draw_bit_on(int x, int y, GContext *ctx) {
-  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_context_set_fill_color(ctx, GColorWhite);
   graphics_fill_rect(ctx, GRect(x, y, OUTER_BOX_SIZE, OUTER_BOX_SIZE), 0, GCornerNone);
 }
 
+// Will draw an 'off' bit, or a square with a hole
 static void draw_bit_off(int x, int y, GContext *ctx) {
   // Draw the entire box
   draw_bit_on(x, y, ctx);
   // Draw the hole in the box
-  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_context_set_fill_color(ctx, GColorBlack);
   graphics_fill_rect(ctx, GRect(x+BOX_OFFSET, y+BOX_OFFSET, INNER_BOX_SIZE, INNER_BOX_SIZE), 0, GCornerNone);
 }
 
+// Draws a bit
 static void draw_bit(Bit b, GContext *ctx) {
   if(b.on == 0)
     draw_bit_off(b.x, b.y, ctx);
@@ -44,6 +50,7 @@ static void draw_bit(Bit b, GContext *ctx) {
     draw_bit_on(b.x, b.y, ctx);
 }
 
+// Finds out which bits are on/off and sets them to respective value
 static void flip_bits_array(Bit *arr, int size, char digit) {
   int i;
   
@@ -55,6 +62,7 @@ static void flip_bits_array(Bit *arr, int size, char digit) {
   }
 }
 
+// Updates the on/off status of bits
 static void update_bits() {
   // Get a tm structure and time digits
   time_t temp = time(NULL);
@@ -87,30 +95,39 @@ static void layer_update_callback(Layer *me, GContext *ctx) {
     draw_bit(g_m0[i], ctx);
 }
 
+// Sets up positioning and states of all bits of given array
 static void setup_bin_arr(Bit* arr, int size, int x, int y_offset) {
   int i;
+  int y_add = OUTER_BOX_SIZE*2;
   
   for(i=0; i < size; ++i) {
     arr[i].x = x;
     arr[i].y = PEB_RES_Y-y_offset;
     arr[i].on = 0;
-    y_offset += 20;
+    y_offset += y_add;
   }
 }
 
+// Calculates positioning for bits and calls helper on each set
 static void setup_binary_arrays() {
-  setup_bin_arr(g_h1, 2, 30, 40);
-  setup_bin_arr(g_h0, 4, 50, 40);
-  setup_bin_arr(g_m1, 3, 70, 40);
-  setup_bin_arr(g_m0, 4, 90, 40);
+  int x_add = OUTER_BOX_SIZE*2;
+  setup_bin_arr(g_h1, 2, X_OFFSET, Y_OFFSET);
+  setup_bin_arr(g_h0, 4, X_OFFSET+x_add, Y_OFFSET);
+  setup_bin_arr(g_m1, 3, X_OFFSET+(x_add*2), Y_OFFSET);
+  setup_bin_arr(g_m0, 4, X_OFFSET+(x_add*3), Y_OFFSET);
 }
 
+// Handles the minute ticks
 static void tick_handler(struct tm *tick_time, TimeUnits units_changes) {
   update_bits();
   layer_mark_dirty(layer);
 }
 
 void init() {
+  // Initialize some constants
+  INNER_BOX_SIZE = OUTER_BOX_SIZE - 4;
+  BOX_OFFSET = (OUTER_BOX_SIZE - INNER_BOX_SIZE) / 2;
+  
   // Setup Binary arrays
   setup_binary_arrays();
 
@@ -121,6 +138,7 @@ void init() {
 int main(void) {
   window = window_create();
   window_stack_push(window, true /* Animated */);
+  window_set_background_color(window, GColorBlack);
 
   // Init the layer for display the image
   Layer *window_layer = window_get_root_layer(window);
