346745fce2f0442fd94031804b0f8c62782c4e42
Anthony <aavina2@gmail.com>
2014-12-08 01:04:59 -0600

    Stable working state
    Subscribed to tick_handler for minute updates. Can probably optimize some parts. Will make it easier to configure between dark|light modes and how big to make the Bits.

--- src/binarypebl.c
+++ src/binarypebl.c
@@ -4,9 +4,7 @@ static Window *window;
 
 static Layer *layer;
 
-static GBitmap *image;
-
-static int PEB_RES_X = 144;
+//static int PEB_RES_X = 144;
 static int PEB_RES_Y = 168;
 
 static int OUTER_BOX_SIZE = 10;
@@ -19,10 +17,12 @@ typedef struct {
   int on; /* 0 = off, anything else = on */
 } Bit;
 
-Bit g_h1[2];
-Bit g_h0[4];
-Bit g_m1[3];
-Bit g_m0[4];
+static Bit g_h1[2];
+static Bit g_h0[4];
+static Bit g_m1[3];
+static Bit g_m0[4];
+
+static const int g_bitmasks[] = {0x01, 0x02, 0x04, 0x08};
 
 static void draw_bit_on(int x, int y, GContext *ctx) {
   graphics_context_set_fill_color(ctx, GColorBlack);
@@ -42,6 +42,32 @@ static void draw_bit(Bit b, GContext *ctx) {
     draw_bit_off(b.x, b.y, ctx);
   else
     draw_bit_on(b.x, b.y, ctx);
+}
+
+static void flip_bits_array(Bit *arr, int size, char digit) {
+  int i;
+  
+  for(i=0; i < size; ++i) {
+    if(digit & g_bitmasks[i])
+      arr[i].on = 1;
+    else
+      arr[i].on = 0;
+  }
+}
+
+static void update_bits() {
+  // Get a tm structure and time digits
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+  int m0_digit = tick_time->tm_min % 10;
+  int m1_digit = (tick_time->tm_min - m0_digit) / 10;
+  int h0_digit = tick_time->tm_hour % 10;
+  int h1_digit = (tick_time->tm_hour - h0_digit) / 10;
+
+  flip_bits_array(g_h1, 2, h1_digit);
+  flip_bits_array(g_h0, 4, h0_digit);
+  flip_bits_array(g_m1, 3, m1_digit);
+  flip_bits_array(g_m0, 4, m0_digit);
 }
 
 static void layer_update_callback(Layer *me, GContext *ctx) {
@@ -79,6 +105,19 @@ static void setup_binary_arrays() {
   setup_bin_arr(g_m0, 4, 90, 40);
 }
 
+static void tick_handler(struct tm *tick_time, TimeUnits units_changes) {
+  update_bits();
+  layer_mark_dirty(layer);
+}
+
+void init() {
+  // Setup Binary arrays
+  setup_binary_arrays();
+
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+}
+
 int main(void) {
   window = window_create();
   window_stack_push(window, true /* Animated */);
@@ -90,12 +129,14 @@ int main(void) {
   layer_set_update_proc(layer, layer_update_callback);
   layer_add_child(window_layer, layer);
 
-  // Setup Binary arrays
-  setup_binary_arrays();
+  init();
+  
+  update_bits();
   
   app_event_loop();
 
-  gbitmap_destroy(image);
+  // Unsubscribe from tick-timer
+  tick_timer_service_unsubscribe();
 
   window_destroy(window);
   layer_destroy(layer);
